7aa3c4a63b3cf6da7b40858a687233950adeabc6
Ousmanly <lyousmane492@gmail.com>
2024-09-18 15:04:51 +0200

    Added list orders

---
+++ src/components/Orders/ListOrders.vue
@@ -0,0 +1,13 @@
+<script setup>
+</script>
+
+<template>
+   <div class="container">
+    <h2>List of Orders</h2>
+  </div>
+</template>
+
+
+<style scoped>
+
+</style>
---
+++ src/views/OrderView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import ListOrders from '@/components/Orders/ListOrders.vue';
+
+
+
+</script>
+
+<template>
+  <ListOrders/>
+</template>
